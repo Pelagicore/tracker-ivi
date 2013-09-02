76afcb036f05bb6838f97813454a308121915843
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-09-02 13:22:13 +0200

    tracker-extract: Vorbis-ivi extractor; Slimmed properties to extract

--- src/tracker-extract/tracker-extract-vorbis-ivi.c
+++ src/tracker-extract/tracker-extract-vorbis-ivi.c
@@ -42,31 +42,11 @@ typedef struct {
 	gchar *artist;
 	gchar *album;
 	gchar *album_artist;
-	gchar *track_count;
 	gchar *track_number;
 	gchar *disc_number;
 	gchar *performer;
-	gchar *track_gain; 
-	gchar *track_peak_gain;
-	gchar *album_gain;
-	gchar *album_peak_gain;
 	gchar *date; 
-	gchar *comment;
-	gchar *genre; 
-	gchar *codec;
-	gchar *codec_version;
-	gchar *sample_rate;
-	gchar *channels; 
-	gchar *mb_album_id; 
-	gchar *mb_artist_id; 
-	gchar *mb_album_artist_id;
-	gchar *mb_track_id; 
-	gchar *lyrics; 
-	gchar *copyright; 
-	gchar *license; 
-	gchar *organization; 
-	gchar *location;
-	gchar *publisher;
+	gchar *genre;
 } VorbisData;
 
 static gchar *
@@ -129,14 +109,10 @@ tracker_extract_get_metadata (TrackerExtractInfo *info)
 		vd.artist = ogg_get_comment (comment, "artist");
 		vd.album = ogg_get_comment (comment, "album");
 		vd.album_artist = ogg_get_comment (comment, "albumartist");
-		vd.track_count = ogg_get_comment (comment, "trackcount");
 		vd.track_number = ogg_get_comment (comment, "tracknumber");
 		vd.disc_number = ogg_get_comment (comment, "DiscNo");
 		vd.performer = ogg_get_comment (comment, "Performer");
 		vd.track_gain = ogg_get_comment (comment, "TrackGain");
-		vd.track_peak_gain = ogg_get_comment (comment, "TrackPeakGain");
-		vd.album_gain = ogg_get_comment (comment, "AlbumGain");
-		vd.album_peak_gain = ogg_get_comment (comment, "AlbumPeakGain");
 
 		date = ogg_get_comment (comment, "date");
 		vd.date = tracker_date_guess (date);
@@ -144,49 +120,11 @@ tracker_extract_get_metadata (TrackerExtractInfo *info)
 
 		vd.comment = ogg_get_comment (comment, "comment");
 		vd.genre = ogg_get_comment (comment, "genre");
-		vd.codec = ogg_get_comment (comment, "Codec");
-		vd.codec_version = ogg_get_comment (comment, "CodecVersion");
-		vd.sample_rate = ogg_get_comment (comment, "SampleRate");
-		vd.channels = ogg_get_comment (comment, "Channels");
-		vd.mb_album_id = ogg_get_comment (comment, "MBAlbumID");
-		vd.mb_artist_id = ogg_get_comment (comment, "MBArtistID");
-		vd.mb_album_artist_id = ogg_get_comment (comment, "MBAlbumArtistID");
-		vd.mb_track_id = ogg_get_comment (comment, "MBTrackID");
-		vd.lyrics = ogg_get_comment (comment, "Lyrics");
-		vd.copyright = ogg_get_comment (comment, "Copyright");
-		vd.license = ogg_get_comment (comment, "License");
-		vd.organization = ogg_get_comment (comment, "Organization");
-		vd.location = ogg_get_comment (comment, "Location");
-		vd.publisher = ogg_get_comment (comment, "Publisher");
 
 		vorbis_comment_clear (comment);
 	}
 
 	md.creator = tracker_coalesce_strip (3, vd.artist, vd.album_artist, vd.performer);
-
-	if (md.creator) {
-		/* NOTE: This must be created before vd.album is evaluated */
-		md.creator_uri = tracker_sparql_escape_uri_printf ("urn:artist:%s", md.creator);
-
-		tracker_sparql_builder_insert_open (preupdate, NULL);
-		if (graph) {
-			tracker_sparql_builder_graph_open (preupdate, graph);
-		}
-
-		tracker_sparql_builder_subject_iri (preupdate, md.creator_uri);
-		tracker_sparql_builder_predicate (preupdate, "a");
-		tracker_sparql_builder_object (preupdate, "ivi:Artist");
-		tracker_sparql_builder_predicate (preupdate, "ivi:artistname");
-		tracker_sparql_builder_object_unvalidated (preupdate, md.creator);
-
-		if (graph) {
-			tracker_sparql_builder_graph_close (preupdate);
-		}
-		tracker_sparql_builder_insert_close (preupdate);
-
-		tracker_sparql_builder_predicate (metadata, "ivi:trackartist");
-		tracker_sparql_builder_object_iri (metadata, md.creator_uri);
-	}
 
 	if (vd.album) {
 		gchar *uri = tracker_sparql_escape_uri_printf ("urn:album:%s", vd.album);
@@ -212,11 +150,6 @@ tracker_extract_get_metadata (TrackerExtractInfo *info)
 		}
 		tracker_sparql_builder_insert_close (preupdate);
 	}
-
-	g_free (vd.track_count);
-	g_free (vd.album_peak_gain);
-	g_free (vd.album_gain);
-	g_free (vd.disc_number);
 
 	if (vd.title) {
 		tracker_sparql_builder_predicate (metadata, "ivi:trackname");
