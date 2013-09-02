6e5c661365954a01cecc390d73369c0cb1b556b6
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-09-02 12:46:30 +0200

    functional-tests: Added support for images in expectation generator

--- tests/functional-tests/create-expectation-from-file.py
+++ tests/functional-tests/create-expectation-from-file.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
-import ConfigParser, sys, argparse, subprocess, StringIO, os.path, glob
+import ConfigParser, sys, argparse, subprocess, StringIO, os.path, glob, time
 from urllib import quote as urlquote
 from pipes import quote
 
-AVPROBE = "/usr/bin/avprobe"
+AVPROBE = "/usr/bin/avprobe -v 0 -show_streams -show_format -of ini %s"
+EXIFTOOL = "exiftool -s %s | sed 's/^\(\w\+\)\s\+:\\s\+/\\1=/g' "+\
+           "| sed '1s/^/[Props]\\n/'"
 AUDIO_FMTS = [".mp3"]
-VIDEO_FMTS = [".avi", ".mkv", ".rm", ".wmv"]
+VIDEO_FMTS = [".avi", ".mkv", ".rm", ".wmv", ".mp4"]
+IMAGE_FMTS = [".jpg", ".jpeg", ".png", ".gif"]
 class MediaTypes:
 	AUDIO = 1
 	VIDEO = 2
+	IMAGE = 3
 
 def uriencode(s):
 	s = urlquote(s, ",'")
@@ -22,28 +26,46 @@ def correctGenre(genre):
 		return "Psychedelic"
 	elif genre == "AlternRock":
 		return "Alt. Rock"
+	elif genre == "Gangsta":
+		return "Gangsta Rap"
 	else: return genre
+
+def avprobeDate(date):
+	return time.strftime("%Y-%m-%dT%H:%M:%SZ",
+	      time.strptime(date, "%Y-%m-%d %H\\:%M\\:%S"))
+
+def exiftoolDate(date):
+	return time.strftime("%Y-%m-%dT%H:%M:%S",
+	      time.strptime(date, "%Y:%m:%d"))
+
+def correctFlash(flash):
+	if flash == "No Flash":
+		return "False"
+	elif flash == "Fired":
+		return "True"
 
 def getMediaType(fname):
 	for ext in AUDIO_FMTS:
-		if fname.endswith(ext):
+		IMAGE_FMTS = [".jpg", ".jpeg", ".png", ".gif"]
+		if fname.lower().endswith(ext.lower()):
 			return MediaTypes.AUDIO
 	for ext in VIDEO_FMTS:
-		if fname.endswith(ext):
+		if fname.lower().endswith(ext.lower()):
 			return MediaTypes.VIDEO
+	for ext in IMAGE_FMTS:
+		if fname.lower().endswith(ext.lower()):
+			return MediaTypes.IMAGE
 	return None
 
-def vals():
-	configParser = ConfigParser.RawConfigParser ()
-	# Make it case sensitive:
-	configParser.optionxform = str
-	configParser.add_section("TestFile")
-	configParser.set("TestFile", "Filename", "Some path")
-	configParser.write(sys.stdout)
-
-def getFileMetadata(f):
-	cmd = "%s -v 0 -show_streams -show_format -of ini %s" %\
-	  (AVPROBE, quote(f))
+def getFileMetadata(f, mediatype):
+	if mediatype == MediaTypes.AUDIO or\
+	   mediatype == MediaTypes.VIDEO:
+		cmd = AVPROBE % quote(f)
+	elif mediatype == MediaTypes.IMAGE:
+		cmd = EXIFTOOL % quote(f)
+	else:
+		print "Bad file type"
+		return
 	confParser = ConfigParser.RawConfigParser()
 	confParser.optionxform = str
 
@@ -61,10 +83,10 @@ def exportProperty(parser1, section1, field1,
 			parser2.add_section(section2)
 		parser2.set(section2, field2, formatter(value))
 		return True
-	print "Field %s is missing from section %s" % (section1, field1)
+	print "Field %s is missing from section %s" % (field1, section1)
 	return False
 
-def setVideoProperties(cf1, cf2):
+def setVideoProperties(cf1, cf2, cmp):
 	success = True
 	filename = os.path.basename(cf1.get("format", "filename"))
 	cf2.add_section("TestFile")
@@ -72,9 +94,12 @@ def setVideoProperties(cf1, cf2):
 	success = exportProperty(cf1, "format.tags", "title",
 	                         cf2, "Metadata", "ivi_videotitle",
 	                         lambda x: x)
+	success = cmp (success, exportProperty(cf1, "format.tags",
+	                         "creation_time", cf2, "Metadata",
+	                         "ivi_filecreated", avprobeDate))
 	return success
 
-def setAudioProperties(cf1, cf2):
+def setAudioProperties(cf1, cf2, cmp):
 	success = True
 	filename = os.path.basename(cf1.get("format", "filename"))
 	cf2.add_section("TestFile")
@@ -82,21 +107,102 @@ def setAudioProperties(cf1, cf2):
 	success = exportProperty(cf1, "format.tags", "title",
 		                 cf2, "Metadata", "ivi_trackname",
 	                         lambda x: x)
-	success = success and exportProperty(cf1, "format.tags", "artist",
+	success = cmp (success, exportProperty(cf1, "format.tags", "artist",
 	                         cf2, "Metadata", "ivi_trackartist",
-	                         lambda x: "<urn:artist:%s>" % uriencode(x))
-	success = success and exportProperty(cf1, "format.tags", "album",
+	                         lambda x: "<urn:artist:%s>" % uriencode(x)))
+	success = cmp (success, exportProperty(cf1, "format.tags", "album",
 	                         cf2, "Metadata", "ivi_trackalbum",
-	                         lambda x: "<urn:album:%s>" % uriencode(x))
-	success = success and exportProperty(cf1, "format.tags", "genre",
+	                         lambda x: "<urn:album:%s>" % uriencode(x)))
+	success = cmp (success, exportProperty(cf1, "format.tags", "genre",
 	                         cf2, "Metadata", "ivi_trackgenre",
-	                         correctGenre)
-	success = success and exportProperty(cf1, "format.tags", "track",
+	                         correctGenre))
+	success = cmp (success, exportProperty(cf1, "format.tags", "track",
 	                         cf2, "Metadata", "ivi_tracktracknumber",
-	                         lambda x: x)
-	success = success and exportProperty(cf1, "format.tags","date",
+	                         lambda x: x))
+	success = cmp (success, exportProperty(cf1, "format.tags","date",
 	                         cf2, "Metadata", "ivi_trackcreated",
-	                         lambda x: "%s-01-01T00:00:00Z" % x)
+	                         lambda x: "%s-01-01T00:00:00Z" % x))
+	return success
+
+def setImageProperties(cf1, cf2, cmp):
+	success = True
+	filename = cf1.get("Props", "FileName")
+	cf2.add_section("TestFile")
+	cf2.set("TestFile", "Filename", filename)
+	extension = filename.split(".")
+	if len(extension) > 1:
+		extension = extension[-1].lower()
+	success = exportProperty(cf1, "Props", "Title",
+	                         cf2, "Metadata", "ivi_imagetitle",
+	                         lambda x: x)
+	if extension is "png":
+		success = cmp (success, exportProperty(cf1, "Props",
+		   "CreationTime", cf2, "Metadata", "ivi_filecreated",
+		   exiftoolDate))
+	if extension is "jpg":
+		success = cmp (success, exportProperty(cf1, "Props",
+		   "DateTimeOriginal", cf2, "Metadata", "ivi_filecreated",
+		   exiftoolDate))
+	else: # If we don't know of this extension, try the Date tag
+		success = cmp (success, exportProperty(cf1, "Props", "Date",
+	                         cf2, "Metadata", "ivi_filecreated",
+	                         exiftoolDate))
+	success = cmp (success, exportProperty(cf1, "Props", "ImageWidth",
+	                         cf2, "Metadata", "ivi_imagewidth",
+	                         lambda x:x))
+	success = cmp (success, exportProperty(cf1, "Props", "ImageHeight",
+	                         cf2, "Metadata", "ivi_imageheight",
+	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "Make",
+#	                         cf2, "Metadata", "ivi_cameramanufacturer",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "Model",
+#	                         cf2, "Metadata", "ivi_cameramodel",
+#	                         lambda x:x))
+	# Image orientation ?
+#	success = cmp (success, exportProperty(cf1, "Props", "Copyright",
+#	                         cf2, "Metadata", "ivi_imagecopyright",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "FNumber",
+#	                         cf2, "Metadata", "ivi_imagefnumber",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "Flash",
+#	                         cf2, "Metadata", "ivi_imageflash",
+#	                         correctFlash))
+#	success = cmp (success, exportProperty(cf1, "Props", "FocalLength",
+#	                         cf2, "Metadata", "ivi_imagefocallength",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "ExposureTime",
+#	                         cf2, "Metadata", "ivi_imageexposuretime",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "ISO",
+#	                         cf2, "Metadata", "ivi_imageisospeed",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "Description",
+#	                         cf2, "Metadata", "ivi_imagedescription",
+#	                         lambda x:x))
+	# Image date ?
+	# Metering mode?
+
+	success = cmp (success, exportProperty(cf1, "Props", "Author",
+	                         cf2, "Metadata", "ivi_imageartist",
+	                         lambda x: "<urn:artist:%s>" % uriencode(x)))
+#	success = cmp (success, exportProperty(cf1, "Props", "Comment",
+#	                         cf2, "Metadata", "ivi_imagecomment",
+#	                         lambda x:x))
+	# Address ?
+#	success = cmp (success, exportProperty(cf1, "Props", "State",
+#	                         cf2, "Metadata", "ivi_imagestate",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "City",
+#	                         cf2, "Metadata", "ivi_imagecity",
+#	                         lambda x:x))
+#	success = cmp (success, exportProperty(cf1, "Props", "Country",
+#	                         cf2, "Metadata", "ivi_imagecountry",
+#	                         lambda x:x))
+	# GPS coordinates ?
+	# Artist vs Creator?
+
 	return success
 
 def getExpectFile(fn):
@@ -107,22 +213,30 @@ def getExpectFile(fn):
 		return fn + ".expected"
 
 def processFile(fn):
-	cf1 = getFileMetadata(fn)
-	cf2 = ConfigParser.RawConfigParser()
-	cf2.optionxform = str
 	ftype = getMediaType(fn)
 	if not ftype:
 		print "Unrecognized file type: %s" % fn
 		return
-	elif ftype == MediaTypes.AUDIO:
-		if not setAudioProperties(cf1, cf2):
-			print "Failed to set audio properties for %s" %\
-			      fn
+
+	cf1 = getFileMetadata(fn, ftype)
+	cf2 = ConfigParser.RawConfigParser()
+	cf2.optionxform = str
+
+	cmp = lambda x,y: x or y
+	if args.picky:
+		cmp = lambda x,y: x and y
+
+	if ftype == MediaTypes.AUDIO:
+		if not setAudioProperties(cf1, cf2, cmp):
+			print "Failed to set audio properties for %s" % fn
 			return
 	elif ftype == MediaTypes.VIDEO:
-		if not setVideoProperties(cf1, cf2):
-			print "Failed to set video properties for %s" %\
-			      fn
+		if not setVideoProperties(cf1, cf2, cmp):
+			print "Failed to set video properties for %s" % fn
+			return
+	elif ftype == MediaTypes.IMAGE:
+		if not setImageProperties(cf1, cf2, cmp):
+			print "Failed to set image properties for %s" % fn
 			return
 	ef = getExpectFile(fn)
 	cf2.write(open(ef,"w"))
@@ -133,6 +247,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("-f", "--file",type=str)
 parser.add_argument("-d", "--dir", type=str)
+parser.add_argument("-p", "--picky", action='store_true', default=False)
 
 args = parser.parse_args()
 
@@ -143,8 +258,11 @@ if __name__ == "__main__":
 	if args.file:
 		processFile(args.file)
 	if args.dir:
-		files = [os.path.join(dirpath, f)
-	                  for dirpath, dirnames, files in os.walk(args.dir)
-	                  for f in files if not f.endswith('.expected')]
-		for f in files:
+		to_process = []
+		for dirpath, dirnames, files in os.walk(args.dir):
+			  dirnames[:] = filter (lambda x: not x.startswith("."),
+					     dirnames)
+			  for f in filter(lambda x: not x.endswith('.expected'), files):
+				to_process.append(os.path.join(dirpath, f))
+		for f in to_process:
 			processFile(f)
