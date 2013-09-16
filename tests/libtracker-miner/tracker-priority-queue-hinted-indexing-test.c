23707d7bac416df474731f279f88231d86857a0e
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-09-16 10:16:37 +0200

    tests: Added criteria counting test to hinted indexing TC

--- tests/libtracker-miner/tracker-priority-queue-hinted-indexing-test.c
+++ tests/libtracker-miner/tracker-priority-queue-hinted-indexing-test.c
@@ -26,6 +26,34 @@
 #include <glib-object.h>
 #include <libtracker-miner/tracker-priority-queue.h>
 
+typedef struct {
+	TrackerPriorityQueue *queue;
+} HintFixture;
+
+typedef struct {
+	gchar **elements;
+	guint numelements;
+} HintParameters;
+
+static HintParameters *init_parameters (gchar **elems, guint numelems)
+{
+	HintParameters *hp = g_malloc (sizeof (HintParameters));
+	hp->elements = elems;
+	hp->numelements = numelems;
+	return hp;
+}
+
+static void fixture_add_elements (HintFixture *fix,
+                                       gconstpointer user_data)
+{
+	fix->queue = tracker_priority_queue_new ();
+	const HintParameters *params = user_data;
+	int i = 0;
+	for (i = 0; i < params->numelements; i++) {
+		tracker_priority_queue_add (fix->queue, params->elements[i], i);
+	}
+}
+
 /*
  * Attempt popping a random item from an empty queue
  */
@@ -76,25 +104,17 @@ static gboolean stringmatcher (TrackerPriorityQueueCriteria *criteria,
                                gpointer user_data) {
 	return g_regex_match_simple (user_data, criteria->node->data, 0,0);
 }
-static void test_priority_queue_hinted_can_prioritize_string ()
+static void test_priority_queue_hinted_can_prioritize_string (HintFixture *fix,
+                                                              gconstpointer user_data)
 {
 	gpointer before = NULL;
 	gpointer after  = NULL;
 	int prio;
-	TrackerPriorityQueue *queue = tracker_priority_queue_new ();
-	gchar *s1 = "_A_";
-	gchar *s2 = "_B_";
-	gchar *s3 = "_C_";
-	gchar *s4 = "_D_";
-	tracker_priority_queue_add (queue, s1, 1);
-	tracker_priority_queue_add (queue, s2, 2);
-	tracker_priority_queue_add (queue, s3, 3);
-	tracker_priority_queue_add (queue, s4, 4);
-	before = tracker_priority_queue_peek (queue, &prio);
-	tracker_priority_queue_prioritize (queue, stringmatcher, 0, ".*C.*");
-	after  = tracker_priority_queue_peek (queue, &prio);
-	g_assert (prio == 0);
-	g_assert (after == s3);
+	before = tracker_priority_queue_peek (fix->queue, &prio);
+	tracker_priority_queue_prioritize (fix->queue, stringmatcher, -1, ".*C.*");
+	after  = tracker_priority_queue_peek (fix->queue, &prio);
+	g_assert (prio == -1);
+	g_assert (g_strcmp0 (after, "_C_") == 0);
 	g_assert (before != after);
 }
 
@@ -106,12 +126,32 @@ static void test_priority_queue_hinted_can_prioritize_empty ()
 	g_assert (tracker_priority_queue_peek (queue, &prio) == NULL);
 }
 
+static void test_priority_queue_hinted_correctly_counts_matches (HintFixture *fix,
+                                                                 gconstpointer user_data)
+{
+	int hits = tracker_priority_queue_prioritize (fix->queue,
+	                                              stringmatcher,
+                                                      -1,
+                                                      "_A.*");
+	int hits2 = tracker_priority_queue_get_num_remaining (fix->queue);
+	int hits3 = tracker_priority_queue_dec_num_remaining (fix->queue);
+	int i = 100;
+	for (; i != 0; i--) {
+		g_assert (tracker_priority_queue_dec_num_remaining (fix->queue) >= 0);
+	}
+
+	g_assert (tracker_priority_queue_get_num_remaining (fix->queue) == 0);
+	g_assert (hits == 2);
+	g_assert (hits == hits2);
+	g_assert (hits-1 == hits3);
+}
 
 int
 main (int    argc,
       char **argv)
 {
 	g_test_init (&argc, &argv, NULL);
+	gchar *testelements[] = {"_A_", "_B_", "_C_", "_D_", "_AB_"};
 
 	g_test_add_func ("/libtracker-miner/tracker-priority-queue-hinted-indexing/empty-random",
 	                 test_priority_queue_hinted_empty_random);
@@ -119,10 +159,14 @@ main (int    argc,
 	                 test_priority_queue_hinted_one_elem_random);
 	g_test_add_func ("/libtracker-miner/tracker-priority-queue-hinted-indexing/popping-is-random",
 	                 test_priority_queue_hinted_popping_is_random);
-	g_test_add_func ("/libtracker-miner/tracker-priority-queue-hinted-indexing/can-prioritize-string",
-	                 test_priority_queue_hinted_can_prioritize_string);
 	g_test_add_func ("/libtracker-miner/tracker-priority-queue-hinted-indexing/can-prioritize-empty",
 	                 test_priority_queue_hinted_can_prioritize_empty);
+	g_test_add ("/libtracker-miner/tracker-priority-queue-hinted-indexing/can-prioritize-string",
+	            HintFixture, init_parameters (testelements, 5), fixture_add_elements,
+	            test_priority_queue_hinted_can_prioritize_string, NULL);
+	g_test_add ("/libtracker-miner/tracker-priority-queue-hinted-indexing/correctly-counts-matches",
+	            HintFixture, init_parameters (testelements, 5), fixture_add_elements,
+	            test_priority_queue_hinted_correctly_counts_matches, NULL);
 
 	return g_test_run ();
 }
